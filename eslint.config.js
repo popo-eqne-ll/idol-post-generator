6b28e826a2aed257d51c88a096d32447f8ea8530
popo-eqne-ll <popo.eqne.ll@gmail.com>
2025-07-09 16:12:47 +0900

    feat: introduce prettier for code formatting
    - Introduce Prettier to enforce consistent code formatting across the project.
    - Integrate Prettier with ESLint to ensure formatting issues are caught during linting.
    - Add a  script to  for easy code formatting.
    - Configure Prettier to align with the existing code style (single quotes, semicolons, 2-space indent, trailing commas).

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import pluginPrettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,6 +16,8 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      configPrettier,
+      pluginPrettier.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
