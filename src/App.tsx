2df3b4b4e39d06c0985a263398b9a11298594fdb
popo-eqne-ll <popo.eqne.ll@gmail.com>
2025-07-22 19:27:25 +0900

    refactor: extract member data into a separate file
    The list of group members, previously defined in App.tsx, has been moved to src/data/groups.ts.
    
    This improves code organization by separating static data from component logic.
    
    Additionally, a "url" field has been added to each member for their X profile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,5 @@
 import { useState, useEffect, type ChangeEvent } from 'react';
-
-interface Member {
-  name: string;
-  account: string;
-}
-
-const groups: Record<string, Member[]> = {
-  イコラブ: [
-    { name: '大谷 映美里', account: 'otani_emiri' },
-    { name: '大場 花菜', account: 'oba_hana_' },
-    { name: '音嶋 莉沙', account: 'otoshima_risa' },
-    { name: '齋藤 樹愛羅', account: 'saitou_kiara' },
-    { name: '佐々木 舞香', account: 'sasaki_maika' },
-    { name: '髙松 瞳', account: 'takamatsuhitomi' },
-    { name: '瀧脇 笙古', account: 'takiwaki_shoko_' },
-    { name: '野口 衣織', account: 'noguchi_iori' },
-    { name: '諸橋 沙夏', account: 'morohashi_sana' },
-    { name: '山本 杏奈', account: 'yamamoto_anna_' },
-  ],
-  ノイミー: [
-    { name: '尾木 波菜', account: 'ogi_hana_' },
-    { name: '落合 希来里', account: 'ochiai_kirari_' },
-    { name: '蟹沢 萌子', account: 'kanisawa_moeko_' },
-    { name: '河口 夏音', account: 'kawaguchi_natsu' },
-    { name: '川中子 奈月心', account: 'kawanagonatsumi' },
-    { name: '櫻井 もも', account: 'sakurai_momo_' },
-    { name: '菅波 美玲', account: 'suganami_mirei_' },
-    { name: '鈴木 瞳美', account: 'suzuki_hitomi_' },
-    { name: '谷崎 早耶', account: 'tanizaki_saya' },
-    { name: '冨田 菜々風', account: 'tomita_nanaka_' },
-    { name: '永田 詩央里', account: 'nagata_shiori_' },
-    { name: '本田 珠由記', account: 'honda_miyuki_' },
-  ],
-  ニアジョイ: [
-    { name: '逢田 珠里依', account: 'aida_jurii' },
-    { name: '天野 香乃愛', account: 'amano_konoa' },
-    { name: '市原 愛弓', account: 'ichihara_ayumi_' },
-    { name: '江角 怜音', account: 'esumi_renon' },
-    { name: '大信田 美月', account: 'oshida_mitsuki' },
-    { name: '大西 葵', account: 'onishi_aoi' },
-    { name: '小澤 愛実', account: 'ozawa_aimi__' },
-    { name: '髙橋 舞', account: 'takahashi_mai__' },
-    { name: '藤沢 莉子', account: 'fujisawa_riko' },
-    { name: '村山 結香', account: 'murayama_yuuka' },
-    { name: '山田 杏佳', account: 'yamada_momoka_' },
-    { name: '山野 愛月', account: 'yamano_arisu' },
-  ],
-};
+import { groups } from './data/groups';
 
 interface FormData {
   liveDate: string;
---
+++ src/data/groups.ts
@@ -0,0 +1,48 @@
+interface Member {
+  name: string;
+  account: string;
+  url: string;
+}
+
+export const groups: Record<string, Member[]> = {
+  イコラブ: [
+    { name: '大谷 映美里', account: 'otani_emiri', url: 'https://x.com/otani_emiri' },
+    { name: '大場 花菜', account: 'oba_hana_', url: 'https://x.com/oba_hana_' },
+    { name: '音嶋 莉沙', account: 'otoshima_risa', url: 'https://x.com/otoshima_risa' },
+    { name: '齋藤 樹愛羅', account: 'saitou_kiara', url: 'https://x.com/saitou_kiara' },
+    { name: '佐々木 舞香', account: 'sasaki_maika', url: 'https://x.com/sasaki_maika' },
+    { name: '髙松 瞳', account: 'takamatsuhitomi', url: 'https://x.com/takamatsuhitomi' },
+    { name: '瀧脇 笙古', account: 'takiwaki_shoko_', url: 'https://x.com/takiwaki_shoko_' },
+    { name: '野口 衣織', account: 'noguchi_iori', url: 'https://x.com/noguchi_iori' },
+    { name: '諸橋 沙夏', account: 'morohashi_sana', url: 'https://x.com/morohashi_sana' },
+    { name: '山本 杏奈', account: 'yamamoto_anna_', url: 'https://x.com/yamamoto_anna_' },
+  ],
+  ノイミー: [
+    { name: '尾木 波菜', account: 'ogi_hana_', url: 'https://x.com/ogi_hana_' },
+    { name: '落合 希来里', account: 'ochiai_kirari_', url: 'https://x.com/ochiai_kirari_' },
+    { name: '蟹沢 萌子', account: 'kanisawa_moeko_', url: 'https://x.com/kanisawa_moeko_' },
+    { name: '河口 夏音', account: 'kawaguchi_natsu', url: 'https://x.com/kawaguchi_natsu' },
+    { name: '川中子 奈月心', account: 'kawanagonatsumi', url: 'https://x.com/kawanagonatsumi' },
+    { name: '櫻井 もも', account: 'sakurai_momo_', url: 'https://x.com/sakurai_momo_' },
+    { name: '菅波 美玲', account: 'suganami_mirei_', url: 'https://x.com/suganami_mirei_' },
+    { name: '鈴木 瞳美', account: 'suzuki_hitomi_', url: 'https://x.com/suzuki_hitomi_' },
+    { name: '谷崎 早耶', account: 'tanizaki_saya', url: 'https://x.com/tanizaki_saya' },
+    { name: '冨田 菜々風', account: 'tomita_nanaka_', url: 'https://x.com/tomita_nanaka_' },
+    { name: '永田 詩央里', account: 'nagata_shiori_', url: 'https://x.com/nagata_shiori_' },
+    { name: '本田 珠由記', account: 'honda_miyuki_', url: 'https://x.com/honda_miyuki_' },
+  ],
+  ニアジョイ: [
+    { name: '逢田 珠里依', account: 'aida_jurii', url: 'https://x.com/aida_jurii' },
+    { name: '天野 香乃愛', account: 'amano_konoa', url: 'https://x.com/amano_konoa' },
+    { name: '市原 愛弓', account: 'ichihara_ayumi_', url: 'https://x.com/ichihara_ayumi_' },
+    { name: '江角 怜音', account: 'esumi_renon', url: 'https://x.com/esumi_renon' },
+    { name: '大信田 美月', account: 'oshida_mitsuki', url: 'https://x.com/oshida_mitsuki' },
+    { name: '大西 葵', account: 'onishi_aoi', url: 'https://x.com/onishi_aoi' },
+    { name: '小澤 愛実', account: 'ozawa_aimi__', url: 'https://x.com/ozawa_aimi__' },
+    { name: '髙橋 舞', account: 'takahashi_mai__', url: 'https://x.com/takahashi_mai__' },
+    { name: '藤沢 莉子', account: 'fujisawa_riko', url: 'https://x.com/fujisawa_riko' },
+    { name: '村山 結香', account: 'murayama_yuuka', url: 'https://x.com/murayama_yuuka' },
+    { name: '山田 杏佳', account: 'yamada_momoka_', url: 'https://x.com/yamada_momoka_' },
+    { name: '山野 愛月', account: 'yamano_arisu', url: 'https://x.com/yamano_arisu' },
+  ],
+};
